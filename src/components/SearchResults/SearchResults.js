b75427924c45276129c9d7e5d1a017c387f97f3d
Karolina Gorlewska <k.gorlewska@gmail.com>
2019-05-24 23:17:52 +0100

    Fix search results

--- src/components/SearchResults/SearchResults.js
+++ src/components/SearchResults/SearchResults.js
@@ -2,22 +2,30 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Card from '../Card/Card';
 import Container from '../Container/Container';
+import styles from './SearchResults.scss';
+//import {getCardsForSearchResults} from '../../redux/cardsRedux';
+
 class SearchResults extends React.Component {
 
   static propTypes = {
     cards: PropTypes.array,
     searchString: PropTypes.string,
+    columns: PropTypes.array,
+  }
+
+  state = {
+    value: this.props.cards || [],
   }
 
   render(){
     const {cards} = this.props;
     return (
 
-      <section>
+      <section className={styles.section}>
         <Container>
-          <div>
+          <div className={styles.component}>
             {cards.map(cardData => (
-              <Card key={cardData.id} {...cardData} />
+              <Card key={cardData.title} {...cardData} />
             ))}
           </div>
         </Container>
--- src/components/SearchResults/SearchResultsContainer.js
+++ src/components/SearchResults/SearchResultsContainer.js
@@ -1,9 +1,10 @@
 import {connect} from 'react-redux';
 import SearchResults from './SearchResults';
 import {getCardsForSearchResults } from '../../redux/cardsRedux.js';
+//import {createAction_changeSearchString} from '../../redux/searchStringRedux.js';
 
-const mapStateToProps = (state) => ({
-  cards: getCardsForSearchResults(state),
+const mapStateToProps = (state, props) => ({
+  cards: getCardsForSearchResults(state, props),
 });
 
 export default connect(mapStateToProps)(SearchResults);
