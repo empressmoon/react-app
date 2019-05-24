464649a79b59a0f753ee6eed65083f8da09708d7
Karolina Gorlewska <k.gorlewska@gmail.com>
2019-05-24 01:19:34 +0100

    Add pages: Info and FAQ; add React Router

---
+++ src/components/Header/Header.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import {NavLink, Link} from 'react-router-dom';
+import styles from './Header.scss';
+import Container from '../Container/Container.js';
+import Icon from '../Icon/Icon.js';
+import {settings} from '../../data/dataStore.js';
+import Search from '../Search/Search';
+
+class Header extends React.Component {
+  render(){
+    return (
+      <header className={styles.component}>
+        <Container>
+          <div className={styles.wrapper}>
+            <Link className={styles.logo} to='/'>
+              <Icon name={settings.header.icon}/>
+            </Link>
+            <Search />
+            <nav>
+              <NavLink exact to='/' activeClassName='active'>Home</NavLink>
+              <NavLink exact to='/info' activeClassName='active'>Info</NavLink>
+              <NavLink exact to='/FAQ' activeClassName='active'>FAQ</NavLink>
+            </nav>
+          </div>
+
+        </Container>
+      </header>
+    );
+  }
+}
+
+export default Header;
