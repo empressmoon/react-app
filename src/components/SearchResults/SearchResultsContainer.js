d9f083e2f42c6dd781ded51541e4b0694dd6507e
Karolina Gorlewska <k.gorlewska@gmail.com>
2019-05-25 17:40:59 +0100

    Fix showing search results after refresh

--- src/components/SearchResults/SearchResultsContainer.js
+++ src/components/SearchResults/SearchResultsContainer.js
@@ -2,9 +2,11 @@ import {connect} from 'react-redux';
 import SearchResults from './SearchResults';
 import {getCardsForSearchResults } from '../../redux/cardsRedux.js';
 //import {getSearchString} from '../../redux/searchStringRedux';
+//import {createAction_changeSearchString} from '../../redux/searchStringRedux';
 
 const mapStateToProps = (state, props) => ({
   cards: getCardsForSearchResults(state, props.match.params.searchString),
+  searchString: props.match.params.searchString,
 });
 
 export default connect(mapStateToProps)(SearchResults);
